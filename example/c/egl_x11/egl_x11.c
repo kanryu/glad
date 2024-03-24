4cd7bca1e77d19e9f5411e53088d1332073c24d1
kanryu <k.kanryu@gmail.com>
2024-03-24 21:14:46 +0900

    egl sample modified with egl, gl2, triangle

--- example/c/egl_x11/egl_x11.c
+++ example/c/egl_x11/egl_x11.c
@@ -1,5 +1,6 @@
 // gcc example/c/egl_x11.c -Ibuild/include build/src/*.c -ldl -lX11
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,8 +11,9 @@
 #include <X11/Xutil.h>
 
 #include <glad/egl.h>
-#include <glad/gles2.h>
-
+//#include <glad/gles2.h>
+#include <glad/gl.h>
+#include "3dmath.h"
 
 const int window_width = 800, window_height = 480;
 
@@ -78,7 +80,8 @@ int main(void) {
     EGLint attr[] = {
         EGL_BUFFER_SIZE, 16,
         EGL_RENDERABLE_TYPE,
-        EGL_OPENGL_ES2_BIT,
+        // EGL_OPENGL_ES2_BIT,
+        EGL_OPENGL_BIT,
         EGL_NONE
     };
 
@@ -102,8 +105,18 @@ int main(void) {
         return 1;
     }
 
+	EGLint ok;
+    ok = eglBindAPI(EGL_OPENGL_API); 
+    if (!ok) {
+		printf("eglBindAPI: bind EGL Api failed (eglError: %d)\n",
+               eglGetError());
+        return 1;
+	}
+
     EGLint ctxattr[] = {
-        EGL_CONTEXT_CLIENT_VERSION, 2,
+        // EGL_CONTEXT_CLIENT_VERSION, 2,
+        EGL_CONTEXT_MAJOR_VERSION_KHR, 2,
+        EGL_CONTEXT_MINOR_VERSION_KHR, 1,
         EGL_NONE
     };
     EGLContext egl_context =
@@ -117,18 +130,26 @@ int main(void) {
     // activate context before loading GL functions using glad
     eglMakeCurrent(egl_display, egl_surface, egl_surface, egl_context);
 
-    int gles_version = gladLoaderLoadGLES2();
-    if (!gles_version) {
-        printf("Unable to load GLES.\n");
+    int gl_version = gladLoaderLoadGL();
+    if (!gl_version) {
+        printf("Unable to load GL.\n");
         return 1;
     }
-    printf("Loaded GLES %d.%d.\n",
-           GLAD_VERSION_MAJOR(gles_version), GLAD_VERSION_MINOR(gles_version));
+    printf("Loaded GL %d.%d\n", GLAD_VERSION_MAJOR(gl_version), GLAD_VERSION_MINOR(gl_version));
+
+    // int gles_version = gladLoaderLoadGLES2();
+    // if (!gles_version) {
+    //     printf("Unable to load GLES.\n");
+    //     return 1;
+    // }
+    // printf("Loaded GLES %d.%d.\n",
+    //        GLAD_VERSION_MAJOR(gles_version), GLAD_VERSION_MINOR(gles_version));
 
     XWindowAttributes gwa;
     XGetWindowAttributes(display, window, &gwa);
     glViewport(0, 0, gwa.width, gwa.height);
 
+    double time_passed = 0.0;
     bool quit = false;
     while (!quit) {
         while (XPending(display)) {
@@ -139,16 +160,39 @@ int main(void) {
                 quit = true;
             }
         }
-
-        glClearColor(0.8, 0.6, 0.7, 1.0);
+        time_passed += 0.0166666;
+        //glClearColor(0.8f, 0.6, 0.7, 1.0);
+        glClearColor(0.0f, 0.0, 0.0, 0.0);
         glClear(GL_COLOR_BUFFER_BIT);
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+        mat4 m = perspective(65.0f, (GLfloat) gwa.width / (GLfloat) gwa.height, 1.0f,
+                            100.0f);
+        glLoadMatrixf(m.d);
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        glRotatef(-90, 1, 0, 0);
+        glTranslatef(0, 0, -1.0f);
+
+        // Draw a rotating colorful triangle
+        glTranslatef(0.0f, 14.0f, 0.0f);
+        glRotatef((GLfloat) time_passed * 100.0f, 0.0f, 0.0f,
+              1.0f);
+        glBegin(GL_TRIANGLES);
+        glColor4f(1.0f, 0.01f, 0.01f, 0.0f);
+        glVertex3f(-5.0f, 0.0f, -4.0f);
+        glColor4f(0.01f, 1.0f, 0.01f, 0.0f);
+        glVertex3f(5.0f, 0.0f, -4.0f);
+        glColor4f(0.01f, 0.01f, 1.0f, 0.0f);
+        glVertex3f(0.0f, 0.0f, 6.0f);
+        glEnd();
 
         eglSwapBuffers(egl_display, egl_surface);
 
         usleep(1000 * 10);
     }
-
-    gladLoaderUnloadGLES2();
+    gladLoaderUnloadGL();
+    //gladLoaderUnloadGLES2();
 
     eglDestroyContext(egl_display, egl_context);
     eglDestroySurface(egl_display, egl_surface);
